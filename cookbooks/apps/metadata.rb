c58cbe54de731f5fdc5ea0deaa05572b846256fa
pigram86 <todd@toddpigram.com>
2013-10-09 08:02:38 -0400

    updated cookbooks

--- cookbooks/apps/metadata.rb
+++ cookbooks/apps/metadata.rb
@@ -1,7 +1,7 @@
 name             'apps'
 maintainer       'Todd Pigram , LLC'
 maintainer_email 'todd@toddpigram.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures apps'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
--- cookbooks/xps/metadata.rb
+++ cookbooks/xps/metadata.rb
@@ -1,7 +1,7 @@
 name             'xps'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer       'Todd pigram, LLC'
+maintainer_email 'todd@toddpigram.com'
+license          'Apache 2.0'
 description      'Installs/Configures xps'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
