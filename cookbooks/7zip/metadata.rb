e0d953e1b16572313511ef17571cc1eae2f544a4
pigram86 <todd@toddpigram.com>
2013-10-14 09:59:42 -0400

    split apps into own cookbooks

---
+++ cookbooks/7zip/metadata.rb
@@ -0,0 +1,8 @@
+name             '7zip'
+maintainer       'Todd Pigram, LLC'
+maintainer_email 'todd@toddpigram.com'
+license          ''
+description      'Installs/Configures 7zip'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends          "windows"
---
+++ cookbooks/acrobat/metadata.rb
@@ -0,0 +1,8 @@
+name             'acrobat'
+maintainer       'Todd Pigram, LLC'
+maintainer_email 'todd@toddpigram.com'
+license          ''
+description      'Installs/Configures acrobat'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends          "windows"
