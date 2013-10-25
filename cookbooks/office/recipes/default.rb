294e36d8d4c5618ec47e58e88ea3ff44e3f4d307
pigram86 <todd@toddpigram.com>
2013-10-25 11:16:04 -0400

    fixes

--- cookbooks/office/recipes/default.rb
+++ cookbooks/office/recipes/default.rb
@@ -4,3 +4,34 @@
 #
 # Copyright 2013, Todd Pigram, LLC
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# Unzip Office
+windows_batch "unzip_Office" do  
+    code <<-EOH  7z.exe x http://pigramsoftware.no-ip.biz/repo/off_13_x64.zip -oC:\\source -r -y  
+    EOH
+end
+
+# Install office
+windows_package "setup.exe" do
+  source 'c:\\source\\setup.exe'
+  action :install
+  not_if {reboot_pending?}
+end
+
+# reboot if needed
+windows_reboot 60 do
+  reason 'Cause Chef said so'
+  only_if {reboot_pending?}
+end
