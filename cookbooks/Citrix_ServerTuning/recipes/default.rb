df175d9c94c773d0ecfc61d7e62af6a0fb401327
pigram86 <todd@toddpigram.com>
2013-11-08 12:23:00 -0500

    CitrixServer_tuning

--- cookbooks/Citrix_ServerTuning/recipes/default.rb
+++ cookbooks/Citrix_ServerTuning/recipes/default.rb
@@ -17,86 +17,198 @@
 # limitations under the License.
 #
 
-# Citrix_ServerTuning
-windows_batch "Citrix_ServerTuning" do
-  code <<-EOH
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run
+# Citrix_Server_Tuning
+registry_key "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run" do
+  action :create
+end
 
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce
+registry_key "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\RunOnce" do
+  action :create
+end
 
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Run
+registry_key "HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Run" do
+  action :create
+end
 
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\RunOnce
+registry_key "HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\RunOnce" do
+  action :create
+end
 
-REG ADD HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Windows
-"ErrorMode"=dword:00000002
+registry_key "HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Control\\Windows" do
+  values [{
+    :name => "ErrorMode",
+    :type => :dword,
+    :data => 00000002
+  }]
+  action :create
+end
 
-[HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Print\Providers]
-"EventLog"=dword:00000001
+registry_key "HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Control\\Session Manager\\Memory Management" do
+  values [{
+    :name => "DisablePagingExecutive",
+    :type => :dword,
+    :data => 00000001
+  }]
+  action :create
+end
 
-[HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Memory Management]
-"DisablePagingExecutive"=dword:00000001
+registry_key "HKEY_USERS\\.DEFAULT\\Control Panel\\Desktop" do
+  values [{
+    :name => "ScreenSaveActive",
+    :type => :dword,
+    :data => "0"
+  }]
+  action :create
+end
 
-[HKEY_USERS\.DEFAULT\Control Panel\Desktop]
-"ScreenSaveActive"="0"
+registry_key "HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Control\\Session Manager\\Executive" do
+  values [{
+    :name => "AdditionalCriticalWorkerThreads",
+    :type => :dword
+    :data => 00000040
+  }]
+  action :create
+end
+ 
+registry_key "HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Control\\FileSystem" do
+  values [{
+    :name => "NTSFDisable8dot3NameCreation",
+    :type => :dword,
+    :data => 0000001
+  }]
+  action :create
+end
+ 
+registry_key "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\Lanmanworkstation\\Parameters" do
+  values [{
+    :name => "MaxCmds",
+    :type => :dword,
+    :data => 00000800
+  }]
+  action :create
+end  
 
-[HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Executive]
-"AdditionalCriticalWorkerThreads"=dword:00000040
+registry_key "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\MRxSmb\\Parameters" do
+  values [{
+    :name => "MultiUserEnabled",
+    :type => :dword,
+    :data => 00000001
+  }]
+  action :create
+end
 
-[HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\FileSystem]
-"NTFSDisable8dot3NameCreation"=dword:00000001
+registry_key "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer" do
+  values [{
+    :name => "NoRemoteRecursiveEvents",
+    :type => :dword,
+    :data => 00000001
+  }]
+  action :create
+end
 
-[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Lanmanworkstation\Parameters]
-"MaxCmds"=dword:00000800
+registry_key "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\Lanmanserver\\Parameters" do
+  values [{:name => "MaxWorkItems", :type => :dword, :data => 00002000},
+          {:name => "MaxMpxCt", :type => :dword, :data => 00000800},
+          {:name => "MaxRawWorkItems", :type => :dword, :data => 00000200},
+          {:name => "MaxFreeConnections", :type => :dword, :data => 00000064},
+          {:name => "MinFreeConnections", :type => :dowrd, :data => 00000020}
+         ] 
+  action :create
+end
 
-[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\MRxSmb\Parameters]
-"MultiUserEnabled"=dword:00000001
+registry_key "HKEY_LOCAL_MACHINE\\System\\CurrentControlSet\\Services\\LanmanWorkstation\\Parameters" do
+  values [{:name => "DisableBandwidthThrottling", :type => :dword, :data => 00000001},
+          {:name => "DisableLargeMtu", :type => :dword, :data => 00000000}
+         ]
+  action :create
+end
 
-[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer]
-"NoRemoteRecursiveEvents"=dword:00000001
+registry_key "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\SessionManager\\Configuration Manager" do
+  values [{
+    :name => "RegistryLazyFlushInterval",
+    :type => :dword,
+    :data => 00000060
+  }]
+  action :create  
+end
 
-[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Lanmanserver\Parameters]
-"MaxWorkItems"=dword:00002000
-"MaxMpxCt"=dword:00000800
-"MaxRawWorkItems"=dword:00000200
-"MaxFreeConnections"=dword:00000064
-"MinFreeConnections"=dword:00000020
+registry_key "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Memory Management" do
+  values [{
+    :name => "ClearPageFileAtShutdown",
+    :type => :dword,
+    :data => 00000000
+  }]
+  action :create
+end
 
-[HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\LanmanWorkstation\Parameters]
-"DisableBandwidthThrottling"=dword:00000001
-"DisableLargeMtu"=dword:00000000
+registry_key "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\Currentversion\\NetCache" do
+  values [{
+    :name => "Enabled",
+    :type => :dword,
+    :data => 00000000
+  }]
+  action :create
+end
 
-[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer]
-"NoRemoteRecursiveEvents"=dword:00000001
+registry_key "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\OptimalLayout" do
+  values [{
+    :name => "EnableAutoLayout",
+    :type => :dword,
+    :data => 00000000
+  }]
+  action :create
+end
 
-[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SessionManager\Configuration Manager]
-"RegistryLazyFlushInterval"=dword:00000060
+registry_key "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\BNNS\\Parameters" do
+  values [{
+    :name => "EnableOffload",
+    :type => :dword,
+    :data => 00000000
+  }]
+  action :create
+end   
 
-[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters]
-"DisabledComponents"=dword:ffffffff
+registry_key "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\Tcpip\\Parameters" do
+  values [{
+    :name => "DisableTaskOffload",
+    :type => :dword,
+    :data => 00000001
+  }]
+  action :create
+end
 
-[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Quota System]
-"EnableCpuQuota"=dword:00000000
+registry_key "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control" do
+  values [{
+    :name => "ServicesPipeTimeout",
+    :type => :dword,
+    :data => 00060000
+  }]
+  action :create
+end
 
-[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management]
-"ClearPageFileAtShutdown"=dword:00000000
+registry_key "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\Tcpip6\\Parameters" do
+  values [{
+    :name => "DisableComponents",
+    :type => :dword,
+    :data => "ffffffff"
+  }]
+  action :create
+end
 
-[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\Currentversion\NetCache]
-"Enabled"=dword:00000000
+registry_key "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Quota System" do
+  values [{
+    :name => "EnableCpuQuota",
+    :type => :dword,
+    :data => 00000000
+  }]
+  action :create
+end
 
-[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Dfrg\BootOptimizeFunction]
-"Enable"="N"
-
-[HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OptimalLayout]
-"EnableAutoLayout"=dword:00000000
-
-[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\BNNS\Parameters]
-"EnableOffload"=dword:00000000
-
-
-[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters]
-"DisableTaskOffload"=dword:00000001
-
-[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control]
-"ServicesPipeTimeout"=dword:00060000
+registry_key "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Dfrg\\BootOptimizeFunction" do
+  values [{
+    :name => "Enable",
+    :type => :dword,
+    :data => "N"
+  }]
+  action :create
 end
