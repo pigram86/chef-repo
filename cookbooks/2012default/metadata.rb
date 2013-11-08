b4d10f2c802b6d362a73bbf3f037eb886a2d1eab
pigram86 <todd@toddpigram.com>
2013-11-08 07:56:01 -0500

    updates

--- cookbooks/2012default/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             '2012default'
-maintainer       'Todd Pigram, LLC'
-maintainer_email 'todd@toddpigram.com'
-license          ''
-description      'Installs/Configures 2012default'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-depends          "windows"
